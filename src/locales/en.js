761c6c2155f43edb471e708309a13c8ec2b6ff1e
yuanle <leoyao12138@gmail.com>
2024-06-27 12:21:52 +0800

    add index image

--- src/locales/en.js
+++ src/locales/en.js
@@ -17,8 +17,8 @@ export default {
         cencel: "Don't wanted"
     },
     home: {
-        title1: 'Hi,',
-        title2: 'This is Yuanle Yao',
+        title1: 'Hi, this is Yuanle Yao,',
+        title2: 'Omnivorous creature general Full-Stack Engineer',
         button1: 'More',
         part2Title1: 'Now I am working on',
         part2Content: 'Personal Web Page',
@@ -28,8 +28,14 @@ export default {
         title1: 'About Me',
         getResume: 'Get my Resume',
         part1Title: 'Hello, I am Yao Yuanle, a versatile full-stack developer with strong adaptability like an omnivorous animal.',
-        part1Content: 'I am like an omnivore for knowledge. I have an appetite for everything. I am interested in discovering and learning any new knowledge From: Hangzhou Zhejiang China',
+        part1Content: `I am like an omnivore for knowledge. I have an appetite for everything. I am interested in discovering 
+        and learning any new knowledge From: Hangzhou Zhejiang China`,
         part2Title: 'Aspire to be a full stack engineer who knows everything.',
-        part2Content: "I am currently enrolled at the University of Technology Sydney in Australia, where I am pursuing my studies. Prior to this, I successfully completed my bachelor's degree in Information Technology from Monash University. Throughout my academic journey in Australia, I have acquired a strong foundation of knowledge and essential skills that are applicable across various fields. Moreover, I have developed the ability to quickly grasp new information and adapt effectively within unfamiliar teams and work environments.",
+        part2Content: `I am currently enrolled at the University of Technology Sydney in Australia, 
+        where I am pursuing my studies. Prior to this, I successfully completed 
+        my bachelor's degree in Information Technology from Monash University. Throughout my academic journey in Australia,
+         I have acquired a strong foundation of knowledge and essential skills that are applicable across various fields. Moreover,
+          I have developed the ability to quickly grasp new information and adapt effectively within unfamiliar teams 
+          and work environments.`,
     }
 }
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,85 +1,85 @@
 <template>
-    <navi-bar></navi-bar>
-    <div class="mainBody">
-        <div class="part1 mb-4">
-            <h2 class="mx-1">{{ $t ('home.title1') }}</h2>
-            <h2>{{ $t ('home.title2') }}</h2>
-            <el-button 
-                 type="success"
-                 @click="toAbout"
-                round>
-                {{ $t ('home.button1') }}
+  <navi-bar></navi-bar>
+  <div class="mainBody">
+    <div class="part1 mb-4">
+        <div style="margin-top: 25px;">
+            <h2 class="mx-1">{{ $t("home.title1") }}</h2>
+            <h3>{{ $t("home.title2") }}</h3>
+            <el-button type="success" @click="toAbout" round>
+              {{ $t("home.button1") }}
             </el-button>
         </div>
-        <div class="part2">
-            <div>
-                <h2>{{ $t ('home.part2Title1') }}</h2>
-            <div>
-                    <el-text class="mb-1" style="padding-right: 20px;">{{ $t ('home.part2Content') }}</el-text>
-                    <el-button type="primary" round @click="toCurrentProject">
-                        {{ $t ('home.button1') }}
-                    </el-button>
-                </div>
-            </div>
-            
-        </div>
-        <div class="ontheWay">
-            {{ $t ('home.stillWork') }}
-        </div>
-        
+          <el-image
+            src="src/assets/person1.png"
+            style="width: 300px; height: 300px;"
+          ></el-image>
     </div>
-    <el-backtop :right="100" :bottom="100" />
-    <footerOne></footerOne>
-
+    <div class="part2">
+      <div>
+        <h3>{{ $t("home.part2Title1") }}</h3>
+        <div>
+          <el-text class="mb-4" style="padding-right: 20px">{{
+            $t("home.part2Content")
+          }}</el-text>
+          <el-button type="primary" round @click="toCurrentProject">
+            {{ $t("home.button1") }}
+          </el-button>
+        </div>
+      </div>
+    </div>
+    <div class="ontheWay">
+      {{ $t("home.stillWork") }}
+    </div>
+  </div>
+  <el-backtop :right="100" :bottom="100" />
+  <footerOne></footerOne>
 </template>
 
 <script setup>
-import NaviBar from '../components/NaviBar.vue';
-import footerOne from '../components/Footer.vue';
-import { useRouter } from 'vue-router';
-const router = useRouter()
+import NaviBar from "../components/NaviBar.vue";
+import footerOne from "../components/Footer.vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
 
-const toAbout=() => {
-    router.push('/about')
-}
-const toCurrentProject=() => {
-    router.push('/current')
-}
-
-
+const toAbout = () => {
+  router.push("/about");
+};
+const toCurrentProject = () => {
+  router.push("/current");
+};
 </script>
 
 <style scoped>
 .mainBody {
-    margin-left: 7rem;
-    margin-right: 7rem;
+  margin-left: 7rem;
+  margin-right: 7rem;
 }
 
 .part1 {
-    display: block;
-    margin: 8px;
-    padding-bottom: 2rem;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 }
 
+
 .part2 {
-    width: 100%;
-    display: flex;
-    justify-content: end;
-    padding: 10px;
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  padding: 10px;
 }
 
 .ontheWay {
-    border: 1px solid #ddd;
-    box-shadow: 5px 5px rgba(155, 155, 155, 0.741);
-    border-radius: 50px;
-    width: 100%;
-    height: 200px;
-    margin-top: 60px;
-    display: flex;
-    justify-content: center;
-    padding-top: 15%;
-    font-size: 25px;
-    text-shadow: 1px 1px rgba(0, 206, 0, 0.696);
+  border: 1px solid #ddd;
+  box-shadow: 5px 5px rgba(155, 155, 155, 0.741);
+  border-radius: 50px;
+  width: 100%;
+  height: 200px;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  padding-top: 15%;
+  font-size: 25px;
+  text-shadow: 1px 1px rgba(72, 79, 72, 0.696);
 }
-
 </style>
